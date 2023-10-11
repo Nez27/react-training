db56b620301dbb2c0bcf7d807d9f77e093b5b5cb
Loi Phan <phanhuuloi15@gmail.com>
2023-10-11 17:35:56 +0700

    Update sample code

--- sample/use-popcorn/src/StarRating.jsx
+++ sample/use-popcorn/src/StarRating.jsx
@@ -10,6 +10,17 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
 };
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
--- sample/use-popcorn/src/main.jsx
+++ sample/use-popcorn/src/main.jsx
@@ -1,28 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css";
-import StarRating from "./StarRating.jsx";
-import { useState } from "react";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} />
-      <p>The move was rated {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <App />
   </React.StrictMode>
 );
