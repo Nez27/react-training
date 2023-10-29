2b04fdb1915dbd48c9220ae75113767753213bc3
Loi Phan <phanhuuloi15@gmail.com>
2023-10-29 21:35:46 +0700

    Remove unnecessary code

--- hotel-management/src/constants/messages.ts
+++ hotel-management/src/constants/messages.ts
@@ -8,6 +8,4 @@ const ERROR_MSG = (errorCode: number, msg: string) => {
   return `Error code: ${errorCode}. Message: ${msg}`;
 };
 
-const TIME_OUT_MSG = 'Connection time out!';
-
-export { INVALID_FORMAT_MSG, ADD_SUCCESS, ERROR_MSG, TIME_OUT_MSG };
+export { INVALID_FORMAT_MSG, ADD_SUCCESS, ERROR_MSG };
--- hotel-management/src/constants/statusCode.ts
+++ hotel-management/src/constants/statusCode.ts
@@ -1,7 +1,6 @@
 const STATUS_CODE = {
   CREATE: 201,
   NOT_FOUND: 404,
-  CONNECTION_TIME_OUT: 522,
 };
 
 export { STATUS_CODE };
